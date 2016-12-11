b8a568ed591660069aaa0dad7fe64203791a6dca
vijay <vijay.sali@gmail.com>
2016-12-11 18:13:30 +0530

    unit_tests: glucose_meter

--- test/models/glucose_meter_test.rb
+++ test/models/glucose_meter_test.rb
@@ -1,7 +1,34 @@
 require 'test_helper'
 
 class GlucoseMeterTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+  end
+
+  def teardown
+    GlucoseMeter.destroy_all
+  end
+
+  def test_creation
+    first_obj = GlucoseMeter.create(level: 2.2)
+    assert_equal 2.2, first_obj.level
+    assert_nil first_obj.user_id
+    assert_equal 1, GlucoseMeter.all.count
+  end
+
+  def test_creation_with_string_char
+    assert_raises ActiveRecord::RecordInvalid do
+      GlucoseMeter.create!(level: "gello")
+    end
+  end
+
+  def test_create_with_blank_level
+    assert_raises ActiveRecord::RecordInvalid do
+      GlucoseMeter.create!(user_id: 1)
+    end
+  end
+
+  def test_check_methods
+  end
+
 end
