72c4b14e1da96aeb1a52af0ff0baf01c8555e56b
vijay <vijay.sali@gmail.com>
2016-12-11 18:13:07 +0530

    methods declared for user and glucose meter

--- app/models/glucose_meter.rb
+++ app/models/glucose_meter.rb
@@ -1,3 +1,10 @@
 class GlucoseMeter < ActiveRecord::Base
   belongs_to :user
+
+  validates :level, presence: true, numericality: {only_float: true}
+
+  scope :today, -> { where(created_at: [DateTime.now.beginning_of_day .. DateTime.now]) }
+  scope :last_month, -> { where(created_at: [DateTime.now - 1.month ..DateTime.now]) }
+  scope :custom_date, -> (st_date, ed_date){ where(created_at: [st_date ..ed_date]) }
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,22 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
   has_many :glucose_meters
+
+  def todays_report
+    self.glucose_meters.today
+  end
+
+  def last_month_report
+    self.glucose_meters.last_month
+  end
+
+  def custom_report(start_date, end_date)
+    start_date = DateTime.parse(start_date) if start_date.is_a? String
+    end_date = DateTie.parse(end_date) if end_date.is_a? String
+
+    self.glucose_meters.custom_date(start_date, end_date)
+  end
+
 end
