dc114376b0208cf6b3493e6a1542be458388a6c7
vijay <vijay.sali@gmail.com>
2016-12-13 02:02:13 +0530

    controller actions declared, new methods added

--- app/controllers/glucose_meters_controller.rb
+++ app/controllers/glucose_meters_controller.rb
@@ -1,74 +1,52 @@
 class GlucoseMetersController < ApplicationController
-  before_action :set_glucose_meter, only: [:show, :edit, :update, :destroy]
+  before_action :set_user, only: [:todays, :last_month, :custom_range]
 
-  # GET /glucose_meters
-  # GET /glucose_meters.json
+  def todays
+    @data = @user.todays_report
+  end
+
+  def last_month
+    @data = @user.last_month_report
+  end
+
+  def custom_range
+  end
+
   def index
-    @glucose_meters = GlucoseMeter.all
+    redirect_to controller: 'glucose_meters', action: 'new'
   end
 
-  # GET /glucose_meters/1
-  # GET /glucose_meters/1.json
-  def show
-  end
 
-  # GET /glucose_meters/new
   def new
     @glucose_meter = GlucoseMeter.new
   end
 
-  # GET /glucose_meters/1/edit
-  def edit
-  end
-
-  # POST /glucose_meters
-  # POST /glucose_meters.json
   def create
     @glucose_meter = GlucoseMeter.new(glucose_meter_params)
-
-    respond_to do |format|
-      if @glucose_meter.save
-        format.html { redirect_to @glucose_meter, notice: 'Glucose meter was successfully created.' }
-        format.json { render :show, status: :created, location: @glucose_meter }
-      else
-        format.html { render :new }
-        format.json { render json: @glucose_meter.errors, status: :unprocessable_entity }
+    user = User.find_by_id(glucose_meter_params["user_id"].to_i)
+    begin
+      user.add_glucose_meter_data(glucose_meter_params["level"].to_f)
+      respond_to do |format|
+        flash[:notice] = "data saved"
+        format.html { render :new, status: :created}
+      end
+    rescue StandardError => e
+      respond_to do |format|
+        flash[:alert] = e.message
+        format.html { render :new, status: :ok}
       end
     end
   end
 
-  # PATCH/PUT /glucose_meters/1
-  # PATCH/PUT /glucose_meters/1.json
-  def update
-    respond_to do |format|
-      if @glucose_meter.update(glucose_meter_params)
-        format.html { redirect_to @glucose_meter, notice: 'Glucose meter was successfully updated.' }
-        format.json { render :show, status: :ok, location: @glucose_meter }
-      else
-        format.html { render :edit }
-        format.json { render json: @glucose_meter.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /glucose_meters/1
-  # DELETE /glucose_meters/1.json
-  def destroy
-    @glucose_meter.destroy
-    respond_to do |format|
-      format.html { redirect_to glucose_meters_url, notice: 'Glucose meter was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_glucose_meter
-      @glucose_meter = GlucoseMeter.find(params[:id])
-    end
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def glucose_meter_params
+    params.require(:glucose_meter).permit(:level, :user_id)
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def glucose_meter_params
-      params.require(:glucose_meter).permit(:level, :references)
-    end
+  def set_user
+    p params
+    @user = User.find_by_id(params[:glucose_meter_id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
 
-  resources :glucose_meters
-  root 'users#dashboard'
+  resources :glucose_meters do
+    get 'todays'
+    get 'last_month'
+    get 'custom_range'
+  end
+  root 'glucose_meters#new'
 
   devise_for :users, path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }
-  #root "layouts#applicaiton"
 
 end
